84bea7174ef403a4cd91469cf6a5eb35fa998633
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2021-08-22 11:41:24 +0200

    feat(): add working impl of rooms delete api

--- server/handlers/homesHandler.go
+++ server/handlers/homesHandler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"net/http"
 	"time"
 
@@ -143,7 +144,7 @@ func (handler *HomesHandler) DeleteHomeHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "Home has been deleted"})
 }
 
-// swagger:operation GET /homes/:id/rooms rooms getRooms
+// swagger:operation GET /homes/{id}/rooms rooms getRooms
 // Returns list of rooms of a home
 // ---
 // produces:
@@ -166,7 +167,7 @@ func (handler *HomesHandler) GetRoomsHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, home.Rooms)
 }
 
-// swagger:operation POST /rooms rooms postRoom
+// swagger:operation POST /homes/{id}/rooms rooms postRoom
 // Create a new room in a home
 // ---
 // produces:
@@ -214,4 +215,132 @@ func (handler *HomesHandler) PostRoomHandler(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "Room added to the home"})
+}
+
+// swagger:operation PUT /homes/{id}/rooms/{rid} rooms putRoom
+// Update an existing room of a home
+// ---
+// parameters:
+// - name: name
+//   floor: number
+// produces:
+// - application/json
+// responses:
+//     '200':
+//         description: Successful operation
+//     '400':
+//         description: Invalid input
+//     '404':
+//         description: Invalid home ID
+func (handler *HomesHandler) PutRoomHandler(c *gin.Context) {
+	id := c.Param("id")
+	objectId, _ := primitive.ObjectIDFromHex(id)
+
+	rid := c.Param("rid")
+	objectRid, _ := primitive.ObjectIDFromHex(rid)
+
+	var room models.Room
+	if err := c.ShouldBindJSON(&room); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var home models.Home
+	err := handler.collection.FindOne(handler.ctx, bson.M{
+		"_id": objectId,
+	}).Decode(&home)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Home not found"})
+		return
+	}
+
+	// search if room is in rooms array
+	var roomFound bool
+	for _, val := range home.Rooms {
+		if val.ID == objectRid {
+			roomFound = true
+		}
+	}
+	if !roomFound {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Room not found"})
+		return
+	}
+
+	// update room
+	filter := bson.D{primitive.E{Key: "_id", Value: objectId}}
+	arrayFilters := options.ArrayFilters{Filters: bson.A{bson.M{"x._id": objectRid}}}
+	upsert := true
+	opts := options.UpdateOptions{
+		ArrayFilters: &arrayFilters,
+		Upsert:       &upsert,
+	}
+	update := bson.M{
+		"$set": bson.M{
+			"rooms.$[x].name":       room.Name,
+			"rooms.$[x].floor":      room.Floor,
+			"rooms.$[x].modifiedAt": time.Now(),
+		},
+	}
+	_, err2 := handler.collection.UpdateOne(handler.ctx, filter, update, &opts)
+	if err2 != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Cannot update room"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Room has been updated"})
+}
+
+// swagger:operation DELETE /homes/{id}/rooms/{rid} rooms deleteRoom
+// Delete an existing room for a home
+// ---
+// produces:
+// - application/json
+// responses:
+//     '200':
+//         description: Successful operation
+//     '404':
+//         description: Invalid room ID
+func (handler *HomesHandler) DeleteRoomHandler(c *gin.Context) {
+	id := c.Param("id")
+	objectId, _ := primitive.ObjectIDFromHex(id)
+
+	rid := c.Param("rid")
+	objectRid, _ := primitive.ObjectIDFromHex(rid)
+
+	var home models.Home
+	err := handler.collection.FindOne(handler.ctx, bson.M{
+		"_id": objectId,
+	}).Decode(&home)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Home not found"})
+		return
+	}
+
+	// search if room is in rooms array
+	var roomFound bool
+	for _, val := range home.Rooms {
+		if val.ID == objectRid {
+			roomFound = true
+		}
+	}
+	if !roomFound {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Room not found"})
+		return
+	}
+
+	// delete room by id
+	filter := bson.D{primitive.E{Key: "_id", Value: objectId}}
+	update := bson.M{
+		"$pull": bson.M{
+			"rooms": bson.D{primitive.E{Key: "_id", Value: objectRid}},
+		},
+	}
+	_, err2 := handler.collection.UpdateOne(handler.ctx, filter, update)
+
+	if err2 != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Cannot update room"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Room has been deleted"})
 }
