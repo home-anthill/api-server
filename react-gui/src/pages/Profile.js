e3f5672764d9cab6106503d0bfdcade94ac87a05
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2021-11-22 02:07:34 +0100

    feat(gui): update profile page

--- react-gui/src/pages/Profile.js
+++ react-gui/src/pages/Profile.js
@@ -3,14 +3,15 @@ import { useLocation } from 'react-router-dom';
 
 export default function Profile() {
   const {state} = useLocation();
-
-  const profile = state.profile?.user;
+  const profile = state.profile?.profile;
 
   return (
     <div className="App">
       <h1>Profile</h1>
-      <p>{profile.login}</p>
-      <p>{profile.name}</p>
+      <p>Login: {profile.github?.login}</p>
+      <p>Name: {profile.github?.name}</p>
+      <p>Email: {profile.github?.email}</p>
+      <img src={profile.github?.avatarURL} />
     </div>
   )
 }
