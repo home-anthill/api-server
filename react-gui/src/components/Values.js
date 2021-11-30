35a4f71f1943d2e02cad20561692fac298426481
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2021-11-30 16:15:40 +0100

    feat(react-gui): add getValues and update values page to load and change ac values

--- react-gui/src/components/Values.js
+++ react-gui/src/components/Values.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-export default function Values ({device}) {
+export default function Values({device}) {
   let token = localStorage.getItem('token');
   let headers = {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer ' + token
   };
 
+  const [value, setValue] = useState({});
+
+  useEffect(() => {
+    async function fn() {
+      console.log('useEffect 1 - fn');
+      const token = localStorage.getItem('token');
+      const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      };
+      try {
+        const response = await axios.get(`http://localhost:8082/api/devices/${device.id}/values`, {
+          headers
+        })
+        const data = response.data;
+        console.log('Values: ', data);
+        setValue(data);
+      } catch (err) {
+        console.error('Cannot get homes');
+      }
+    }
+
+    fn();
+  }, []);
+
   async function setOnOff() {
     const response = await axios.post(`http://localhost:8082/api/devices/${device.id}/values/onoff`, {
-      on: true
+      on: value.on
     }, {
       headers
     })
@@ -20,7 +45,7 @@ export default function Values ({device}) {
 
   async function setTemperature() {
     const response = await axios.post(`http://localhost:8082/api/devices/${device.id}/values/temperature`, {
-      temperature: 25
+      temperature: value.temperature
     }, {
       headers
     })
@@ -30,7 +55,7 @@ export default function Values ({device}) {
 
   async function setMode() {
     const response = await axios.post(`http://localhost:8082/api/devices/${device.id}/values/mode`, {
-      mode: 1
+      mode: value.mode
     }, {
       headers
     })
@@ -40,7 +65,7 @@ export default function Values ({device}) {
 
   async function setFanMode() {
     const response = await axios.post(`http://localhost:8082/api/devices/${device.id}/values/fanmode`, {
-      fan: 2
+      fanMode: value.fanMode
     }, {
       headers
     })
@@ -48,9 +73,9 @@ export default function Values ({device}) {
     console.log('response', data);
   }
 
-  async function setFanSwing() {
-    const response = await axios.post(`http://localhost:8082/api/devices/${device.id}/values/fanswing`, {
-      swing: true
+  async function setFanSpeed() {
+    const response = await axios.post(`http://localhost:8082/api/devices/${device.id}/values/fanspeed`, {
+      fanSpeed: value.fanSpeed
     }, {
       headers
     })
@@ -58,17 +83,59 @@ export default function Values ({device}) {
     console.log('response', data);
   }
 
-  // TODO add current values (probably with a get via REST to api-server and than to api-devices via gRPC to get device[i].status
   return (
-    <div>
-      <h1>Device</h1>
-      <p>{device.name} - {device.manufacturer} - {device.model}</p>
-      <br/>
-      <button onClick={setOnOff}>Set On/Off</button>
-      <button onClick={setTemperature}>Set Temperature</button>
-      <button onClick={setMode}>Set Mode</button>
-      <button onClick={setFanMode}>Set Fan mode</button>
-      <button onClick={setFanSwing}>Set Fan swing</button>
-    </div>
+    <>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={value.on}
+            onChange={event => setValue(Object.assign({}, value, {on: event.target.checked}))}/>
+          On
+        </label>
+        <br/>
+        <button onClick={setOnOff}>Set On/Off</button>
+      </div>
+      <div>
+        <input
+          value={value.temperature}
+          onChange={event => setValue(Object.assign({}, value, {temperature: +(event.target.value)}))}
+          type="number"
+          placeholder="Temperature"
+        />
+        <br/>
+        <button onClick={setTemperature}>Set Temperature</button>
+      </div>
+      <div>
+        <input
+          value={value.mode}
+          onChange={event => setValue(Object.assign({}, value, {mode: +(event.target.value)}))}
+          type="number"
+          placeholder="Mode"
+        />
+        <br/>
+        <button onClick={setMode}>Set Mode</button>
+      </div>
+      <div>
+        <input
+          value={value.fanMode}
+          onChange={event => setValue(Object.assign({}, value, {fanMode: +(event.target.value)}))}
+          type="number"
+          placeholder="Fan mode"
+        />
+        <br/>
+        <button onClick={setFanMode}>Set Fan mode</button>
+      </div>
+      <div>
+        <input
+          value={value.fanSpeed}
+          onChange={event => setValue(Object.assign({}, value, {fanSpeed: +(event.target.value)}))}
+          type="number"
+          placeholder="Fan speed"
+        />
+        <br/>
+        <button onClick={setFanSpeed}>Set Fan speed</button>
+      </div>
+    </>
   );
 }
