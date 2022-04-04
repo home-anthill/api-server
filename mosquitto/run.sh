0e7b651c66954386559578bb720f7d7c76403ead
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2022-04-04 21:02:57 +0200

    fix(mosquitto): try to fix moquitto container

--- mosquitto/run.sh
+++ mosquitto/run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 mkdir certs
 cp /etc/letsencrypt/archive/ac-ks89.eu/cert1.pem /certs/cert.pem
 cp /etc/letsencrypt/archive/ac-ks89.eu/fullchain1.pem /certs/fullchain.pem
@@ -8,10 +10,14 @@ cp /etc/letsencrypt/archive/ac-ks89.eu/privkey1.pem /certs/privkey.pem
 chgrp mosquitto /certs/cert.pem
 chgrp mosquitto /certs/fullchain.pem
 chgrp mosquitto /certs/privkey.pem
-chmod g+r /certs/cert.pem
-chmod g+r /certs/fullchain.pem
-chmod g+r /certs/privkey.pem
+chmod 444 /certs/cert.pem
+chmod 444 /certs/fullchain.pem
+chmod 444 /certs/privkey.pem
 
 ls -la /certs
 
-mosquitto -c /mosquitto/config/mosquitto.conf
+sleep 2
+
+mosquitto -v -c /mosquitto/config/mosquitto.conf
+
+sleep infinity
