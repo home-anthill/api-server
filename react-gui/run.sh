6cef69d6825f6e40ff7dc111007fb3a20e3ce1a9
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2022-04-08 20:45:36 +0200

    feat(gui): add env variables to remove static emails, paths and domains from run.sh

--- react-gui/run.sh
+++ react-gui/run.sh
@@ -1,35 +1,52 @@
 #!/bin/sh
+set -e
 
+# Let's encrypt certificates
 #Certificate is saved at: /etc/letsencrypt/live/<DOMAIN>/fullchain.pem
 #Key is saved at:         /etc/letsencrypt/live/<DOMAIN>/privkey.pem
 
-if [[ -f /home/nginx-conf/nginx.conf ]]
+# Default nginx.conf location loaded by nginx
+nginx_conf_file=/etc/nginx/nginx.conf
+
+# if not defined, apply default production server
+CERTBOT_SERVER=${CERTBOT_SERVER:?"https://acme-v02.api.letsencrypt.org/directory"}
+
+# read env variables
+echo "Env variables:"
+echo "CERTBOT_EMAIL = ${CERTBOT_EMAIL}"
+echo "CERTBOT_DOMAIN = ${CERTBOT_DOMAIN}"
+echo "CERTBOT_SERVER = ${CERTBOT_SERVER}"
+echo "BASE_NGINX_CONF_FILEPATH = ${BASE_NGINX_CONF_FILEPATH}"
+echo "NGINX_CONF_HOSTPATH = ${NGINX_CONF_HOSTPATH}"
+
+if [ -f "${NGINX_CONF_HOSTPATH}" ]
 then
   echo "nginx.conf already in persistent volume '/home/nginx-conf/'"
   echo "Copying nginx.conf from persistent volume to the final destination"
-  cp /home/nginx-conf/nginx.conf /etc/nginx/nginx.conf
+  cp "${NGINX_CONF_HOSTPATH}" "${nginx_conf_file}"
   # certbot renew --dry-run
   # Check here to setup chrontab: https://eff-certbot.readthedocs.io/en/stable/using.html#setting-up-automated-renewal
   # cronjob
   # 0 12 * * * /usr/bin/certbot renew --quiet
 
-  # start nginx with default config file '/etc/nginx/nginx.conf'
+  # start nginx with default config file '${nginx_conf_file}'
   # and wait thanks to "daemon off;".
   # Taken from official docker image https://github.com/nginxinc/docker-nginx/blob/92973a30900b2ed881d208d10cadade34bbbab33/Dockerfile-alpine.template#L123
   nginx -g "daemon off;"
 else
   echo "nginx.conf not available in persistent volume '/home/nginx-conf/'"
+  echo "Copying basic nginx.conf without SSL to the final destination"
+  cp "${BASE_NGINX_CONF_FILEPATH}" "${nginx_conf_file}"
 
-  cp /home/nginx.conf /etc/nginx/nginx.conf
-
+  echo "Calling certbot to get certificates and update ${nginx_conf_file} with SSL configuration"
   # --- get let's encrypt certificate from production server (https://letsencrypt.org/docs/rate-limits/)
-  certbot --nginx -m stefano.cappa.ks89@gmail.com --agree-tos -d ac-ks89.eu,www.ac-ks89.eu -n --server https://acme-v02.api.letsencrypt.org/directory
+  certbot --nginx -m "${CERTBOT_EMAIL}" --agree-tos -d ${CERTBOT_DOMAIN},www.${CERTBOT_DOMAIN} -n --server "${CERTBOT_SERVER}"
 
   # --- get let's encrypt certificate from staging server (https://letsencrypt.org/docs/staging-environment/)
-  # certbot --nginx -m stefano.cappa.ks89@gmail.com --agree-tos -d ac-ks89.eu,www.ac-ks89.eu -n --server https://acme-staging-v02.api.letsencrypt.org/directory
+  # certbot --nginx -m "${CERTBOT_EMAIL}" --agree-tos -d ${CERTBOT_DOMAIN},www.${CERTBOT_DOMAIN} -n --server https://acme-staging-v02.api.letsencrypt.org/directory
 
-  echo "Copying nginx.conf from persistent volume to the final destination"
-  cp /etc/nginx/nginx.conf /home/nginx-conf/nginx.conf
+  echo "Copying nginx.conf updated by certbot to the persistent volume"
+  cp "${nginx_conf_file}" "${NGINX_CONF_HOSTPATH}"
 
   # IMPORTANT
   # certbot already starts nginx automatically, so I quit and restart nginx to be sure that everything will be ok
