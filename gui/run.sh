823b4718dc86ef3b12bedf97ae93d665fbea4bf8
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2022-05-17 19:25:31 +0200

    feat(gui): remove certbot renew with crontab, because managed by the dedicated container

--- gui/run.sh
+++ gui/run.sh
@@ -22,34 +22,6 @@ if [ ${CERTBOT_EMAIL+x} ] && [ ${CERTBOT_DOMAIN+x} ] && [ ${CERTBOT_SERVER+x} ];
   #Certificate is saved at: /etc/letsencrypt/live/<DOMAIN>/fullchain.pem
   #Key is saved at:         /etc/letsencrypt/live/<DOMAIN>/privkey.pem
 
-  # https://techjogging.com/create-letsencrypt-certificate-alpine-nginx.html
-  echo "Checking 'crond' existence"
-  rc-service --list | grep -i crond
-  echo "Checking running services"
-  rc-status -a
-  sleep 5
-  # add syslog (required by crontab)
-  echo "Enabling syslog"
-  rc-update add syslog boot
-  sleep 2
-  echo "Preparing syslog"
-  touch /run/openrc/softlevel
-  echo "Starting syslog"
-  rc-service syslog start
-  sleep 5
-  # start crontab
-  echo "Starting crond"
-  rc-update add crond default
-  sleep 2
-  rc-service crond start
-  sleep 5
-  # check services
-  echo "Checking running services again"
-  rc-status -a
-  crontab -l
-  # to read log messages
-  # tail -f /var/log/messages
-
   if [ -f "${NGINX_CONF_HOSTPATH}" ]
   then
     echo "nginx.conf already in persistent volume '/home/nginx-conf/'"
