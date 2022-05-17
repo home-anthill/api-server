ed96da95866e0273244dbc1057a1cb2ea0e6eb48
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2022-05-17 19:25:17 +0200

    feat(mosquitto): remove certbot renew with crontab, because managed by the dedicated container

--- mosquitto/upgrade-certbot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-touch /ac/upgrade-certbot.log
-
-# https://techjogging.com/create-letsencrypt-certificate-alpine-nginx.html
-
-echo "Calling 'apk update certbot certbot-nginx openssl'..." >> /ac/upgrade-certbot.log
-apk update certbot certbot-nginx openssl
-echo "Certbot updated!" >> /ac/upgrade-certbot.log
