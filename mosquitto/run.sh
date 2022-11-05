cc9767bc76aa7a72bada5893cfc39255121c5f47
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2022-11-05 01:50:22 +0100

    fix(mosquitto): try to fix previous commit with crontab

--- mosquitto/run.sh
+++ mosquitto/run.sh
@@ -12,34 +12,6 @@ if [ ${CERTBOT_EMAIL+x} ] && [ ${CERTBOT_DOMAIN+x} ] && [ ${CERTBOT_SERVER+x} ];
   echo "CERTBOT_DOMAIN = ${CERTBOT_DOMAIN}"
   echo "CERTBOT_SERVER = ${CERTBOT_SERVER}"
 
-  # https://techjogging.com/create-letsencrypt-certificate-alpine-nginx.html
-  echo "Checking 'crond' existence"
-  rc-service --list | grep -i crond
-  echo "Checking running services"
-  rc-status -a
-  sleep 5
-  # add syslog (required by crontab)
-  echo "Enabling syslog"
-  rc-update add syslog boot
-  sleep 2
-  echo "Preparing syslog"
-  touch /run/openrc/softlevel
-  echo "Starting syslog"
-  rc-service syslog start
-  sleep 5
-  # start crontab
-  echo "Starting crond"
-  rc-update add crond default
-  sleep 2
-  rc-service crond start
-  sleep 5
-  # check services
-  echo "Checking running services again"
-  rc-status -a
-  crontab -l
-  # to read log messages
-  # tail -f /var/log/messages
-
   if [ -d "/etc/letsencrypt/live/${CERTBOT_DOMAIN}" ]
   then
     echo "Certificates already exists"
