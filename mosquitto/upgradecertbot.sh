decabf6bee541d32e63a24b02cb6216de7750831
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2022-11-06 01:04:49 +0100

    fix(mosquitto+gui): improve pervious commits about certbot renew - temporary remove the renew command to test if everything work as expected

--- mosquitto/upgradecertbot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-touch /ac/upgradecertbot.log
-
-# https://techjogging.com/create-letsencrypt-certificate-alpine-nginx.html
-
-echo "Calling 'apk update certbot certbot-nginx openssl'..." >> /ac/upgradecertbot.log
-apk update certbot openssl
-echo "Certbot updated!" >> /ac/upgradecertbot.log
