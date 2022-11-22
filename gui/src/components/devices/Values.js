720edb8f26e1599d751d5199fed11c9d3bc0f77e
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2022-11-22 18:35:52 +0100

    feat(gui): add snackbar in device controller to show success or error

--- gui/src/components/devices/Values.js
+++ gui/src/components/devices/Values.js
@@ -1,17 +1,31 @@
 import React, { useEffect, useState } from 'react';
 
-import { Button, FormControl, FormControlLabel, Switch, TextField } from '@mui/material';
+import { Button, Snackbar, FormControl, FormControlLabel, Switch, TextField } from '@mui/material';
+import MuiAlert from '@mui/material/Alert';
 
 import './Values.css';
 
 import { getApi, postApi } from '../../apis/api';
 
+const STATE_SUCCESS = 'Device state update successfully!';
+const STATE_ERROR = 'Cannot update device state!';
+
 export default function Values({device}) {
   const [onOff, setOnOff] = useState(false);
   const [temperature, setTemperature] = useState(0);
   const [mode, setMode] = useState(0);
-  const [fanMode, setFanMode] = useState(0);
   const [fanSpeed, setFanSpeed] = useState(0);
+  const [swing, setSwing] = useState(false);
+  const [snackBarState, setSnackBarState] = useState({
+    open: false,
+    severity: 'success',
+    message: STATE_SUCCESS
+  });
+
+  // use a custom Alert extending MuiAlert instead of the Alert defined in @mui/material
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
 
   useEffect(() => {
     async function fn() {
@@ -20,44 +34,30 @@ export default function Values({device}) {
         setOnOff(response.on);
         setTemperature(response.temperature);
         setMode(response.mode);
-        setFanMode(response.fanMode);
         setFanSpeed(response.fanSpeed);
+        setSwing(response.swing);
       } catch (err) {
-        console.error('Cannot get homes');
+        console.error('Cannot get device values');
       }
     }
 
     fn();
-  }, []);
+  }, [device.id]);
 
-  async function postOnOff() {
-    await postApi(`/api/devices/${device.id}/values/onoff`, {
-      on: onOff
-    });
-  }
-
-  async function postTemperature() {
-    await postApi(`/api/devices/${device.id}/values/temperature`, {
-      temperature: temperature
-    });
-  }
-
-  async function postMode() {
-    await postApi(`/api/devices/${device.id}/values/mode`, {
-      mode: mode
-    });
-  }
-
-  async function postFanMode() {
-    await postApi(`/api/devices/${device.id}/values/fanmode`, {
-      fanMode: fanMode
-    });
-  }
-
-  async function postFanSpeed() {
-    await postApi(`/api/devices/${device.id}/values/fanspeed`, {
-      fanSpeed: fanSpeed
-    });
+  async function postValues() {
+    try {
+      await postApi(`/api/devices/${device.id}/values`, {
+        on: onOff,
+        temperature: temperature,
+        mode: mode,
+        fanSpeed: fanSpeed,
+        swing: swing,
+      });
+      setSnackBarState({...snackBarState, open: true, severity: 'success', message: STATE_SUCCESS});
+    } catch(err) {
+      console.log('Cannot set device values. Err = ', err);
+      setSnackBarState({...snackBarState, open: true, severity: 'error', message: STATE_ERROR});
+    }
   }
 
   function handleOnOffChange(event) {
@@ -72,21 +72,23 @@ export default function Values({device}) {
     setMode(+(event.target.value));
   }
 
-  function handleFanModeChange(event) {
-    setFanMode(+(event.target.value));
-  }
-
   function handleFanSpeedChange(event) {
     setFanSpeed(+(event.target.value));
   }
 
+  function handleSwingChange(event) {
+    setSwing(event.target.checked);
+  }
+
+  const handleSnackbarClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setSnackBarState({...snackBarState, open: false });
+  };
+
   return (
     <div className="Values">
-
-      <div className="ValueContainer">
-
-      </div>
-
       <div className="ValueContainer">
         <FormControlLabel
           control={
@@ -96,12 +98,7 @@ export default function Values({device}) {
           }
           label="On/Off"
         />
-        <Button sx={{
-                  marginLeft: '10px'
-                }}
-                onClick={postOnOff}>Send</Button>
       </div>
-
       <div className="ValueContainer">
         <FormControl>
           <TextField label="Temperature"
@@ -109,12 +106,7 @@ export default function Values({device}) {
                    value={temperature}
                    onChange={e => handleTemperatureChange(e)}/>
         </FormControl>
-        <Button sx={{
-                  marginLeft: '10px'
-                }}
-                onClick={postTemperature}>Send</Button>
       </div>
-
       <div className="ValueContainer">
         <FormControl>
           <TextField label="Mode"
@@ -122,25 +114,7 @@ export default function Values({device}) {
                      value={mode}
                      onChange={e => handleModeChange(e)}/>
         </FormControl>
-        <Button sx={{
-                  marginLeft: '10px'
-                }}
-                onClick={postMode}>Send</Button>
       </div>
-
-      <div className="ValueContainer">
-        <FormControl>
-          <TextField label="FanMode"
-                     variant="outlined"
-                     value={fanMode}
-                     onChange={e => handleFanModeChange(e)}/>
-        </FormControl>
-        <Button sx={{
-                  marginLeft: '10px'
-                }}
-                onClick={postFanMode}>Send</Button>
-      </div>
-
       <div className="ValueContainer">
         <FormControl>
           <TextField label="FanSpeed"
@@ -148,11 +122,31 @@ export default function Values({device}) {
                      value={fanSpeed}
                      onChange={e => handleFanSpeedChange(e)}/>
         </FormControl>
-        <Button sx={{
-                  marginLeft: '10px'
-                }}
-                onClick={postFanSpeed}>Send</Button>
       </div>
+      <div className="ValueContainer">
+        <FormControlLabel
+          control={
+            <Switch
+              checked={swing}
+              onChange={e => handleSwingChange(e)} />
+          }
+          label="Swing"
+        />
+      </div>
+
+      <Button variant="contained"
+              color="success"
+              sx={{ marginTop: '10px'}}
+              onClick={postValues}>Send</Button>
+
+      <Snackbar open={snackBarState.open}
+                autoHideDuration={2000}
+                onClose={handleSnackbarClose}
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}>
+        <Alert onClose={handleSnackbarClose} severity={snackBarState.severity} sx={{ width: '100%' }}>
+          {snackBarState.message}
+        </Alert>
+      </Snackbar>
     </div>
   );
 }
