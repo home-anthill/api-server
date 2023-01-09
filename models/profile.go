55e77fa45ac1c86b7fb2a69887c851dda5693e7d
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2023-01-09 19:38:46 +0100

    fix: add json tags to Github struct
    Signed-off-by: Stefano Cappa <stefano.cappa.ks89@gmail.com>

--- models/profile.go
+++ models/profile.go
@@ -1,27 +1,27 @@
 package models
 
 import (
-  "go.mongodb.org/mongo-driver/bson/primitive"
-  "time"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
 )
 
 // Github struct
 // no need to define json, because we set this value manually
 // we need onlt bson, because we want to store it into the db as part of Profile
 type Github struct {
-  ID        int64  `bson:"id"`
-  Login     string `bson:"login"`
-  Name      string `bson:"name"`
-  Email     string `bson:"email"`
-  AvatarURL string `bson:"avatarURL"`
+	ID        int64  `json:"id" bson:"id"`
+	Login     string `json:"login" bson:"login"`
+	Name      string `json:"name" bson:"name"`
+	Email     string `json:"email" bson:"email"`
+	AvatarURL string `json:"avatarURL" bson:"avatarURL"`
 }
 
 type Profile struct {
-  ID         primitive.ObjectID   `json:"id" bson:"_id"`
-  Github     Github               `json:"github" bson:"github"`
-  ApiToken   string               `json:"apiToken" bson:"apiToken"`
-  Devices    []primitive.ObjectID `json:"devices" bson:"devices"`
-  Homes      []primitive.ObjectID `json:"homes" bson:"homes"`
-  CreatedAt  time.Time            `json:"createdAt" bson:"createdAt"`
-  ModifiedAt time.Time            `json:"modifiedAt" bson:"modifiedAt"`
+	ID         primitive.ObjectID   `json:"id" bson:"_id"`
+	Github     Github               `json:"github" bson:"github"`
+	ApiToken   string               `json:"apiToken" bson:"apiToken"`
+	Devices    []primitive.ObjectID `json:"devices" bson:"devices"`
+	Homes      []primitive.ObjectID `json:"homes" bson:"homes"`
+	CreatedAt  time.Time            `json:"createdAt" bson:"createdAt"`
+	ModifiedAt time.Time            `json:"modifiedAt" bson:"modifiedAt"`
 }
